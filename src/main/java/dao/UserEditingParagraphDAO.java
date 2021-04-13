1f9adfdb0c4d9df01bfa19883df765d6e5ebd7f3
Nicolas Serrand <nicolas.serrand@grenoble-inp.org>
2021-04-13 10:52:19 +0200

    bouton "annuler la modification" dans la modifcation d'un paragraphe

--- src/main/java/dao/UserEditingParagraphDAO.java
+++ src/main/java/dao/UserEditingParagraphDAO.java
@@ -51,6 +51,39 @@ public class UserEditingParagraphDAO extends AbstractDataBaseDAO {
     }
 
     /**
+     * Renvoie le paragraphe (IdBook, numParagraph) si il est édité par idUser.
+     */
+    public Paragraph getParagraph(int idUser, int idBook, int numParagraph) {
+        List<Integer> result = new ArrayList<Integer>();
+        try (
+                 Connection conn = getConn();  PreparedStatement st = conn.prepareStatement("SELECT * FROM UserEditingParagraph JOIN Paragraph ON "
+                        + "UserEditingParagraph.idBook = Paragraph.IdBook "
+                        + "AND UserEditingParagraph.numParagraph = Paragraph.numParagraph "
+                        + "WHERE idUser = ? AND Paragraph.numParagraph = ? AND Paragraph.idBook = ? ");) {
+            st.setInt(1, idUser);
+            st.setInt(2, numParagraph);
+            st.setInt(3, idBook);
+            ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                return new Paragraph(
+                        rs.getInt("idBook"),
+                        rs.getInt("numParagraph"),
+                        rs.getString("paragraphTitle"),
+                        rs.getString("text"),
+                        rs.getString("author"),
+                        rs.getBoolean("isEnd"),
+                        rs.getBoolean("isValidate"),
+                        rs.getBoolean("isAccessible")
+                );
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DAOException("Erreur BD dans UserEditingParagraphDAO (GetParagraph) " + e.getMessage(), e);
+        }
+    }
+    
+    /**
      * Ajoute un ligne disant qu'un utilisateur est en train d'éditer un paragraphe.
      */
     public void addEditing(int idUser, int idBook, int numParagraph) {
